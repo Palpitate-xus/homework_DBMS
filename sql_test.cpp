3076e3b95161f80cfa53c684f4b4dc9a102b3366
Palpitate-xus <xus1758011080@outlook.com>
2023-01-08 11:38:35 +0800

    完成insert语句解析

--- sql_test.cpp
+++ sql_test.cpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 using namespace std;
 
+// sql语句预处理
 string sql_processor(string raw_sql)
 {
     // 大写转小写
@@ -15,6 +16,7 @@ string sql_processor(string raw_sql)
     return raw_sql;
 }
 
+// sql语句解析
 string parser(string sql)
 {
     if(sql.substr(0, 2) == "use") {
@@ -23,33 +25,41 @@ string parser(string sql)
         }
     }
     if(sql.substr(0, 5) == "create") {
+        // create标准格式: create database [databasename]
         if (sql.substr(6, 13) == "database")
         {
+            return sql.substr(14, sql.size());  // 返回数据库名称
             // 扫描空格
-            int pos[20];
-            int count = 0;
-            for (int i = 12; i < sql.size(); i++)
-            {
-                if (sql[i] == ' ')
-                {
-                pos[count] = i; // 记录空格的位置
-                count++;
-                }
-            }
+            // int pos[20];
+            // int count = 0;
+            // for (int i = 12; i < sql.size(); i++)
+            // {
+            //     if (sql[i] == ' ')
+            //     {
+            //         pos[count] = i; // 记录空格的位置
+            //         count++;
+            //     }
+            // }
             // 扫描空格之间的字符
-            for (int i = 0; i < count; i++)
-            {
-                for (int j = pos[i]; j < pos[i+1]; j++)
-                {
-                    /* code */
-                }
+            // for (int i = 0; i < count; i++)
+            // {
+            //     for (int j = pos[i]; j < pos[i+1]; j++)
+            //     {
+            //         /* code */
+            //     }
                 
-            }
+            // }
             
         }
+        // create标准格式：create table [tablename]
         if (sql.substr(6, 13) == "table")
         {
-            /* code */
+            return sql.substr(14, sql.size());  // 返回表名称
+        }
+        // 可能会有索引
+        if (sql.substr(6, 13) == "index")
+        {
+            return sql.substr(14, sql.size());  // 这个待完成
         }
     }
     if(sql.substr(0, 10) == "insert into") {
@@ -62,9 +72,52 @@ string parser(string sql)
                 pos[count] = i; // 记录空格的位置
                 count++;
             }
+            // 扫描空格之间的字符
+            // [tablename], [column], values, [values]
+            string tokens[5];
+            for (int i = 0; i < count; i++)
+            {
+                tokens[i] = sql.substr(pos[i], pos[i+1]);
+            }
+            string tablename = tokens[0];
+            string raw_column = tokens[1].substr(1, tokens[1].size()-1);  // 去除首尾空格
+            string raw_values = tokens[3].substr(1, tokens[3].size()-1);  // 去除首尾空格
+
+            int colcount = 0;  // 标记列数
+            int col_pos[99999];  // 记录列中','的位置
+            int valuescount = 0;  // 标记给定数值的个数
+            int val_pos[99999];  // 记录数值中','的位置
+            for (int i = 0; i < raw_column.size(); i++)
+            {
+                if (raw_column[i] == ',') {
+                    col_pos[colcount] = i;
+                    colcount++;
+                }
+            }
+            for (int i = 0; i < raw_values.size(); i++)
+            {
+                if (raw_values[i] == ',')
+                {
+                    val_pos[valuescount] = i;
+                    valuescount++;
+                }
+            }
+            if (colcount != valuescount)
+            {
+                return "cols is not equal to values";
+            }
+
+            string cols[99999];
+            string values[99999];
+            for (int i = 0; i < colcount; i++)
+            {
+                cols[i] = raw_column.substr(col_pos[i], col_pos[i+1]);  // 提取列名
+                values[i] = raw_values.substr(val_pos[i], val_pos[i+1]);  // 提取数值
+            }
         }
     }
     if(sql.substr(0, 10) == "delete from") {
+        // 标准delete语句：delete from [tablename] where [xxx]
         int pos[20];
         int count = 0;
         for (int i = 12; i < sql.size(); i++)
