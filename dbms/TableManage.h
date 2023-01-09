bb4552676ea1675450bd407b38309e711158d247
Palpitate-xus <xus1758011080@outlook.com>
2023-01-09 18:25:41 +0800

    整合文件

---
+++ dbms/TableManage.h
@@ -0,0 +1,191 @@
+#include<bits/stdc++.h>
+#include<direct.h>
+#include"DateType.h"
+using namespace std;
+#define maxn 100010
+typedef int ll;
+struct column{
+    bool isNull=0;
+    string dataType="",dataName="";
+    ll dsize=0;
+    void print(){
+        cout<<"ColumnName: "<<dataName<<'\n';
+        cout<<"hasNull: "<<isNull<<'\n';
+        cout<<"DataType: "<<dataType<<'\n';
+        cout<<"DataSize: "<<dsize<<'\n'<<endl;
+    }
+};
+struct table{
+    column cols[30];
+    ll len=0;
+    string tablename="";
+    void append(column ncol){cols[len++]=ncol;}
+    void print(){cout<<tablename<<'\n'<<endl;for(ll i=0;i<len;i++)cols[i].print();}
+    ll size(){ll otc=0;for(ll i=0;i<len;i++)otc+=cols[i].dsize;return otc;}
+};
+struct strlist{
+    string str[30];
+    ll len=0;
+    void print(){for(ll i=0;i<len;i++)cout<<str[i]<<endl;}
+    void append(string nstr){str[len++]=nstr;}
+};
+struct intlist{
+    //
+};
+ll createDatabase(string DBname){
+    if(!_access(DBname.c_str(),0))return 1;//database already exist
+    _mkdir(DBname.c_str());
+    FILE*xx=fopen((DBname+"\\"+"carve.log").c_str(),"wb");
+    fclose(xx);
+    xx=fopen((DBname+"\\"+"tlist.lst").c_str(),"wb");
+    fclose(xx);
+    return 0;//success
+}
+ll dropDatabase(string DBname){
+    if(_access(DBname.c_str(),0))return 1;//database not exist
+    system(("del "+DBname+" /q").c_str());
+    _rmdir(DBname.c_str());
+    return 0;//success
+}
+ll createTable(string DBname,string Tablename,table tbl){
+    string filepath=DBname+"\\"+Tablename;
+    if(!_access((filepath+".stc").c_str(),0)||_access(DBname.c_str(),0))return 1;//table already exist or database not exist
+    FILE*xx=fopen((filepath+".stc").c_str(),"wb");
+    fwrite(&tbl.len,4,1,xx);
+    for(ll i=0;i<tbl.len;i++){
+        fwrite(&tbl.cols[i].isNull,1,1,xx);
+        fwrite(tbl.cols[i].dataType.c_str(),4,1,xx);
+        fwrite(tbl.cols[i].dataName.c_str(),15,1,xx);
+        fwrite(&tbl.cols[i].dsize,4,1,xx);
+    }
+    fclose(xx);
+    xx=fopen((filepath+".dt").c_str(),"wb");
+    fclose(xx);
+    xx=fopen((DBname+"\\tlist.lst").c_str(),"ab");
+    fwrite(Tablename.c_str(),15,1,xx);
+    fclose(xx);
+    return 0;//success
+}
+ll dropTable(string DBname,string Tablename){
+    string filepath=DBname+"\\"+Tablename;strlist tbls;char t[15];
+    if(_access((filepath+".stc").c_str(),0))return 1;//table not exist
+    system(("del "+filepath+".stc,"+filepath+".dt /q").c_str());
+    FILE*xx=fopen((DBname+"\\tlist.lst").c_str(),"rb");
+    while(fread(t,15,1,xx)){
+        string tem=string(t);
+        if(tem!=Tablename)tbls.append(tem);
+    }
+    fclose(xx);
+    xx=fopen((DBname+"\\tlist.lst").c_str(),"wb");
+    for(ll i=0;i<tbls.len;i++)fwrite(tbls.str[i].c_str(),15,1,xx);
+    fclose(xx);
+    return 0;//success
+}
+strlist getTablenames(string DBname){
+    FILE*xx=fopen((DBname+"\\tlist.lst").c_str(),"rb");
+    char t[16];strlist tbls;
+    while(fread(t,15,1,xx))tbls.append(string(t));
+    fclose(xx);
+    return tbls;
+}
+table getTablestruction(string DBname,string Tablename){
+    string filepath=DBname+"\\"+Tablename+".stc";table t={.tablename=Tablename};
+    if(_access(filepath.c_str(),0))return t;//table not exist
+    FILE*xx=fopen(filepath.c_str(),"rb");
+    fread(&t.len,4,1,xx);
+    for(ll i=0;i<t.len;i++){
+        char type[5]="",name[16]="";
+        fread(&t.cols[i].isNull,1,1,xx);
+        fread(type,4,1,xx);
+        fread(name,15,1,xx);
+        fread(&t.cols[i].dsize,4,1,xx);
+        t.cols[i].dataType=string(type);
+        t.cols[i].dataName=string(name);
+    }
+    fclose(xx);
+    return t;
+}
+column setint(string columnName,bool isNull,ll scale){
+    string tem;
+    if(scale<=1)tem="tiny";
+    else if(scale==2)tem="int",scale=4;
+    else if(scale>=3)tem="long",scale=8;
+    return {.isNull=isNull,.dataType=tem,.dataName=columnName,.dsize=scale};
+}
+column setstring(string columnName,bool isNull,ll length){
+    return {.isNull=isNull,.dataType="char",.dataName=columnName,.dsize=max(1,min(length,1005))};
+}
+column setdate(string columnName,bool isNull){
+    return {.isNull=isNull,.dataType="date",.dataName=columnName,.dsize=12};
+}
+ll insert(string DBname,string Tablename,strlist values){
+    string filepath=DBname+"\\"+Tablename;
+    if(_access((filepath+".stc").c_str(),0))return 1;//table not exist
+    table tbl=getTablestruction(DBname,Tablename);
+    FILE*xx=fopen((filepath+".dt").c_str(),"ab");
+    fseek(xx,0,2);ll size=ftell(xx);
+    map<string,string>key;
+    for(ll i=0;i<values.len;i++){
+        string tem=values.str[i];ll pos=tem.find(' ',0);
+        if(pos==-1)return 1;
+        key[tem.substr(0,pos)]=tem.substr(pos+1,tem.length()-pos);
+    }
+    for(ll i=0;i<tbl.len;i++){
+        column tem=tbl.cols[i];string str=key[tem.dataName];
+        if(!tem.isNull&&str=="")goto l;//unexpected NULL value
+        if(tem.dataType=="char")fwrite(str.c_str(),tem.dsize,1,xx);
+        else if(tem.dataType=="date"){
+            Date t=Date(str.c_str());
+            if(!t.year)goto l;//wrong date form
+            fwrite(&t,12,1,xx);
+        }
+        else{
+            ll len=str.length(),t=0;
+            if(len>19)goto l;//value string too long
+            for(ll i=0;i<len;i++){
+                if(str[i]<48||str[i]>57)goto l;//has not number characters
+                t=t*10+str[i]-48;
+            }
+            fwrite(&t,tem.dsize,1,xx);
+        }
+    }
+    fclose(xx);return 0;
+    l:rewind(xx);
+    _chsize(_fileno(xx),size);//clear written data
+    fclose(xx);return 1;
+}
+ll delet(string DBname,string Tablename,strlist cstr){
+    string filepath=DBname+"\\"+Tablename;
+    if(_access((filepath+".stc").c_str(),0))return 1;//table not exist
+    table tbl=getTablestruction(DBname,Tablename);ll size=tbl.size();
+    FILE*xx=fopen((filepath+".dt").c_str(),"rb");
+    map<string,string>key;
+    while(1){
+        //
+    }
+    fclose(xx);
+    return 0;
+}
+int main(){
+    /*createDatabase("info");
+    table newt;
+    newt.append(setint("goodsCount",0,2));
+    newt.append(setstring("goodsName",0,15));
+    newt.append(setdate("saleDate",0));
+    createTable("info","goods",newt);*/
+    /*strlist nstr;
+    nstr.append("goodsCount 15");
+    nstr.append("goodsName earphones");
+    nstr.append("saleDate 2020-08-19");
+    insert("info","goods",nstr);*/
+    /*FILE*xx=fopen("info\\goods.dt","rb");
+    while(1){
+        ll ct=0;char gn[16]="";Date sd;
+        fread(&ct,4,1,xx);
+        fread(gn,15,1,xx);
+        fread(&sd,12,1,xx);
+        if(feof(xx))break;
+        cout<<ct<<','<<gn<<','<<sd<<endl;
+    }
+    fclose(xx);*/
+}
---
+++ dbms/sql_parser.h
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+struct strlist{
+    string str[30];
+    int len = 0;
+    void print()
+    {
+        for(int i = 0; i < len; i++)
+        cout << str[i] << endl;
+    }
+    void append(string nstr)
+    {
+        str[len++] = nstr;
+    }
+};
+
+// 本文件用于SQL语句的解析
+
+// sql语句预处理
+string sql_processor(string raw_sql)
+{
+    // 大写转小写
+    for(auto &i : raw_sql){
+        i = tolower(i);
+    }
+    raw_sql.erase(remove(raw_sql.begin(), raw_sql.end(), '\n'),raw_sql.end());  // 删除换行符
+    raw_sql.erase(remove(raw_sql.begin(), raw_sql.end(), '\t'),raw_sql.end());  // 删除制表符
+    return raw_sql;
+}
+
+// sql语句解析
+string parser(string sql)
+{
+    if(sql.substr(0, 2) == "use") {
+        if(sql.substr(4, 11) == "database") {
+            return sql.substr(12, sql.size());  // 返回数据库名称
+        }
+    }
+    if(sql.substr(0, 5) == "create") {
+        // create标准格式: create database [databasename]
+        if (sql.substr(6, 13) == "database")
+        {
+            return sql.substr(14, sql.size());  // 返回数据库名称
+            // 扫描空格
+            // int pos[20];
+            // int count = 0;
+            // for (int i = 12; i < sql.size(); i++)
+            // {
+            //     if (sql[i] == ' ')
+            //     {
+            //         pos[count] = i; // 记录空格的位置
+            //         count++;
+            //     }
+            // }
+            // 扫描空格之间的字符
+            // for (int i = 0; i < count; i++)
+            // {
+            //     for (int j = pos[i]; j < pos[i+1]; j++)
+            //     {
+            //         /* code */
+            //     }
+                
+            // }
+            
+        }
+        // create标准格式：create table [tablename]
+        if (sql.substr(6, 13) == "table")
+        {
+            return sql.substr(14, sql.size());  // 返回表名称
+        }
+        // 可能会有索引
+        if (sql.substr(6, 13) == "index")
+        {
+            return sql.substr(14, sql.size());  // 这个待完成
+        }
+    }
+    if(sql.substr(0, 10) == "insert into") {
+        int pos[20];
+        int count = 0;
+        for (int i = 12; i < sql.size(); i++)
+        {
+            if (sql[i] == ' ')
+            {
+                pos[count] = i; // 记录空格的位置
+                count++;
+            }
+            // 扫描空格之间的字符
+            // [tablename], [column], values, [values]
+            string tokens[5];
+            for (int i = 0; i < count; i++)
+            {
+                tokens[i] = sql.substr(pos[i], pos[i+1]);
+            }
+            string tablename = tokens[0];
+            string raw_column = tokens[1].substr(1, tokens[1].size()-1);  // 去除首尾空格
+            string raw_values = tokens[3].substr(1, tokens[3].size()-1);  // 去除首尾空格
+
+            int colcount = 0;  // 标记列数
+            int col_pos[99999];  // 记录列中','的位置
+            int valuescount = 0;  // 标记给定数值的个数
+            int val_pos[99999];  // 记录数值中','的位置
+            for (int i = 0; i < raw_column.size(); i++)
+            {
+                if (raw_column[i] == ',') {
+                    col_pos[colcount] = i;
+                    colcount++;
+                }
+            }
+            for (int i = 0; i < raw_values.size(); i++)
+            {
+                if (raw_values[i] == ',')
+                {
+                    val_pos[valuescount] = i;
+                    valuescount++;
+                }
+            }
+            if (colcount != valuescount)
+            {
+                return "cols is not equal to values";
+            }
+
+            string cols[99999];
+            string values[99999];
+            for (int i = 0; i < colcount; i++)
+            {
+                cols[i] = raw_column.substr(col_pos[i], col_pos[i+1]);  // 提取列名
+                values[i] = raw_values.substr(val_pos[i], val_pos[i+1]);  // 提取数值
+            }
+        }
+    }
+    if(sql.substr(0, 10) == "delete from") {
+        // 标准delete语句：delete from [tablename] where [xxx] and ...
+        int pos[20];
+        int count = 0;
+        for (int i = 12; i < sql.size(); i++)
+        {
+            if (sql[i] == ' ')
+            {
+                pos[count] = i; // 记录空格的位置
+                count++;
+            }
+        }
+        string tokens[99999];
+        for (int i = 0; i < count; i++)
+        {
+            tokens[i] = sql.substr(pos[i], pos[i+1]);
+        }
+        
+        string tablename = tokens[0];  // 提取表名
+        int andnum = count - 3;  // 条件的个数
+        string condition[99999];
+        int condition_count = 0;
+        for (int i = 2; i < count; i+=2)
+        {
+            condition[condition_count] = tokens[i];
+        }
+    }
+    if (sql.substr(0, 5) == "delete")
+    {
+        if (sql.substr(7, 14) == "database")
+        {
+            return sql.substr(16, sql.size());
+        }
+        else if (sql.substr(7, 11) == "table")
+        {
+            return sql.substr(13, sql.size());
+        }
+        else
+        {
+            return "invalid command";
+        }
+    }
+    
+    if(sql.substr(0, 5) == "select") {
+        // 标准的select语句：select [*]/[column] from [tablename] where [conditions]
+        int pos[20];
+        int count = 0;
+        for (int i = 6; i < sql.size(); i++)
+        {
+            if (sql[i] == ' ')
+            {
+                pos[count] = i; // 记录空格的位置
+                count++;
+            }
+        }
+        string tokens[99999];
+        for (int i = 0; i < count; i++)
+        {
+            tokens[i] = sql.substr(pos[i], pos[i+1]);
+        }
+        string raw_result = tokens[0];  // select出的列
+        string raw_tablename = tokens[2];  // 表名
+        string raw_conditions = tokens[4];  // select的条件
+        string result[99999];
+        // 如果select非全部列
+        if (raw_result != "*")
+        {
+            int col_count = 0;
+            int result_pos[99999];
+            for (int i = 0; i < raw_result.size(); i++)
+            {
+                if (raw_result[i] == '*')
+                {
+                    result_pos[col_count] = i;
+                    col_count++;
+                }
+            }
+            for (int i = 0; i < col_count; i++)
+            {
+                result[i] = raw_result.substr(result_pos[i], result_pos[i+1]);
+            }  
+        }
+        
+        string conditions[99999];
+        int con_count = 0;
+        for (int i = 5; i < count; i++)
+        {
+            conditions[con_count] = tokens[i];
+            con_count++;
+        }
+    }
+    if(sql.substr(0, 5) == "update") {
+        // 标准的update语句：update [tablename] set [updates] where [conditions]
+        int pos[20];
+        int count = 0;
+        for (int i = 6; i < sql.size(); i++)
+        {
+            if (sql[i] == ' ')
+            {
+                pos[count] = i; // 记录空格的位置
+                count++;
+            }
+        }
+        string tokens[99999];
+        for (int i = 0; i < count; i++)
+        {
+            tokens[i] = sql.substr(pos[i], pos[i+1]);
+        }
+
+        string tablename = tokens[0];
+        string raw_updates = tokens[2];
+        string raw_conditions = tokens[4];
+
+        string updates[99999];
+        string conditions[99999];
+        int updates_pos[99999];  // update中","的位置
+        int conditions_pos[99999];  // conditions中","的位置
+        int updates_count = 0;
+        int conditions_count = 0;
+        for (int i = 0; i < raw_updates.size(); i++)
+        {
+            if (raw_updates[i] == ',')
+            {
+                updates_pos[updates_count] = i;
+                updates_count++;
+            }
+        }
+        for (int i = 0; i < raw_conditions.size(); i++)
+        {
+            if (raw_conditions[i] == ',')
+            {
+                conditions_pos[conditions_count] = i;
+                conditions_count++;
+            }
+        }
+        updates[0] = raw_updates.substr(0, updates_pos[0]);
+        for (int i = 0; i < updates_count; i++)
+        {
+            updates[i+1] = raw_updates.substr(updates_pos[i], updates_pos[i+1]);
+        }
+        conditions[0] = raw_conditions.substr(0, conditions_pos[0]);
+        for (int i = 0; i < count; i++)
+        {
+            conditions[i+1] = raw_conditions.substr(conditions_pos[i], conditions_pos[i+1]);
+        }
+    }
+    if(sql.substr(0, 4) == "alter") {
+        // 或许会有这个功能
+    }
+}
+
+int main()
+{
+    string sql_input;
+    cin >> sql_input;
+    sql_input = sql_processor(sql_input);
+    parser(sql_input);
+    system("pause");
+}
