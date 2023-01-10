167bcd48a253062d713ff2dc09bbe4b5cca0c8f8
Palpitate-xus <xus1758011080@outlook.com>
2023-01-10 20:18:34 +0800

    update permissions.cpp

--- permissions.cpp
+++ permissions.cpp
@@ -11,20 +11,70 @@ struct user{
 };
 
 user users[99999];
+int user_count = 0;
 
+// 登录函数，返回值1表示登陆成功，0表示不存在该用户，-1表示密码错误。
 int login(string username, string password)
 {
+    // 读取users数据
     string data;
-    ifstream infile; 
-    infile.open("user.dat"); 
-    infile >> data;
-    cout << data;
+    ifstream infile;
+    infile.open("user.dat");
+    while (!infile.eof())
+    {
+        infile >> data;
+        users[user_count].username = data;
+        infile >> data;
+        users[user_count].password = data;
+        infile >> data;
+        users[user_count].permission = data;
+        user_count++;
+    }
+    for (int i = 0; i < user_count; i++)
+    {
+        if (username == users[i].username)
+        {
+            if (password == users[i].password)
+            {
+                infile.close();
+                return 1;
+            }
+            else
+            {
+                infile.close();
+                return -1;
+            }
+        }
+    }
     infile.close();
     return 0;
 }
 
+// 用户权限查询函数
 int permissionQuery(string username)
 {
+    for (int i = 0; i < user_count; i++)
+    {
+        if (username == users[i].username)
+        {
+            if (users[i].permission == "admin")
+            {
+                return 1;
+            }
+            else if (users[i].permission == "user")
+            {
+                return 0;
+            }
+        }
+    }
+}
+
+// 创建用户
+int createUser(user new_user)
+{
+    fstream fs("user.dat", ios::binary | ios::out | ios::app);
+    fs << new_user.username << " " << new_user.password << " " << new_user.permission << endl;
+    fs.close();
     return 0;
 }
 
@@ -48,17 +98,18 @@ int main ()
 //    outfile.close();
  
    // 以读模式打开文件
-   ifstream infile; 
-   infile.open("user.dat"); 
+//    ifstream infile; 
+//    infile.open("user.dat"); 
  
-   cout << "Reading from the file" << endl; 
-   infile >> data; 
+//    cout << "Reading from the file" << endl; 
+//    infile >> data; 
  
    // 在屏幕上写入数据
-   cout << data << endl;
+//    cout << data << endl;
    
    // 关闭打开的文件
-   infile.close();
+//    infile.close();
+    login("test", "test");
     system("pause");
    return 0;
 }
